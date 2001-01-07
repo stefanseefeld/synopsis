9a79ecaa60148963a72d309e6fd48b689f645862
chalky <chalky@91adf076-48d4-0310-a9e2-da78070693fa>
2001-01-07 05:41:44 +0000

    Adding demo/Python directory.

---
+++ demo/Python/test.py
@@ -0,0 +1,19 @@
+# Test
+
+def foo():
+    pass
+
+def func(alpha, beta = 4*2):
+    pass
+
+class Base1:
+    """A base class"""
+    pass
+
+class Base2:
+    """Another base"""
+    pass
+
+class FooClass (Base1, Base2):
+    """A derived class"""
+    pass
