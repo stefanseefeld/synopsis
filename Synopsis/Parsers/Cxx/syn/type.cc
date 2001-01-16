eec3468fc1d1e26acf4fc8c01a1bfbecee88650a
chalky <chalky@91adf076-48d4-0310-a9e2-da78070693fa>
2001-01-16 04:26:03 +0000

    All methods implemented.

--- Synopsis/Parsers/Cxx/syn/type.cc
+++ Synopsis/Parsers/Cxx/syn/type.cc
@@ -2,7 +2,113 @@
 
 #include "type.hh"
 
+/*
+using Type::Named;
+using Type::Base;
+using Type::Forward;
+using Type::Declared;
+using Type::Template;
+using Type::Modifier;
+using Type::Visitor;
+using Type::Parameterized;
+using Type::FuncPtr;
+*/
+
+Type::Type::Type() {}
+Type::Type::~Type() {}
+void Type::Type::accept(Visitor* visitor)
+{
+    visitor->visitType(this);
+}
+
 using namespace Type;
+
+Named::Named(Name n)
+    : m_name(n)
+{
+}
+
+void Named::accept(Visitor* visitor)
+{
+    visitor->visitNamed(this);
+}
+
+Base::Base(Name n)
+    : Named(n)
+{
+}
+
+
+void Base::accept(Visitor* visitor)
+{
+    visitor->visitBase(this);
+}
+
+Forward::Forward(Name n)
+    : Named(n)
+{
+}
+
+
+void Forward::accept(Visitor* visitor)
+{
+    visitor->visitForward(this);
+}
+
+Declared::Declared(Name n, AST::Declaration* decl)
+    : Named(n), m_decl(decl)
+{
+}
+
+
+void Declared::accept(Visitor* visitor)
+{
+    visitor->visitDeclared(this);
+}
+
+Template::Template(Name n, AST::Declaration* decl, Type::vector_t& params)
+    : Declared(n, decl), m_params(params)
+{
+}
+
+
+void Template::accept(Visitor* visitor)
+{
+    visitor->visitTemplateType(this);
+}
+
+Modifier::Modifier(Type* alias, Mods& pre, Mods& post)
+    : m_alias(alias), m_pre(pre), m_post(post)
+{
+}
+
+
+void Modifier::accept(Visitor* visitor)
+{
+    visitor->visitModifier(this);
+}
+
+Parameterized::Parameterized(Template* t, Type::vector_t& params)
+    : m_template(t), m_params(params)
+{
+}
+
+
+void Parameterized::accept(Visitor* visitor)
+{
+    visitor->visitParameterized(this);
+}
+
+FuncPtr::FuncPtr(Type::Type* ret, Mods& premods, Type::vector_t& params)
+    : m_return(ret), m_premod(premods), m_params(params)
+{
+}
+
+
+void FuncPtr::accept(Visitor* visitor)
+{
+    visitor->visitFuncPtr(this);
+}
 
 //
 // Type::Visitor
@@ -12,5 +118,11 @@ using namespace Type;
 Visitor::~Visitor() {}
 void Visitor::visitType(Type*) {}
 void Visitor::visitForward(Forward *t) { visitType(t); }
-void Visitor::visitBase(Forward *t) { visitType(t); }
-void Visitor::viistDeclared(Declared *t) { visitType(t); }
+void Visitor::visitBase(Base *t) { visitNamed(t); }
+void Visitor::visitDeclared(Declared *t) { visitNamed(t); }
+void Visitor::visitModifier(Modifier *t) { visitType(t); }
+void Visitor::visitNamed(Named *t) { visitType(t); }
+void Visitor::visitTemplateType(Template *t) { visitDeclared(t); }
+void Visitor::visitParameterized(Parameterized *t) { visitType(t); }
+void Visitor::visitFuncPtr(FuncPtr *t) { visitType(t); }
+
