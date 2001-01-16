07d9ad7e789c9e6d00d287e4bfd43cd85ac4058c
chalky <chalky@91adf076-48d4-0310-a9e2-da78070693fa>
2001-01-16 02:31:08 +0000

    Initial commit

---
+++ Synopsis/Parsers/Cxx/syn/dumper.hh
@@ -0,0 +1,33 @@
+// File: dumper.hh
+// Dumper is a visitor similar to Formatter/DUMP.py
+
+#ifndef H_SYNOPSIS_CPP_DUMPER
+#define H_SYNOPSIS_CPP_DUMPER
+
+#include "ast.hh"
+#include "type.hh"
+
+//. Dumper displays the AST to the screen
+class Dumper : public AST::Visitor, public Type::Visitor {
+public:
+    Dumper();
+
+    void visit(const vector<AST::Declaration*>&);
+    virtual void visitDeclaration(AST::Declaration*);
+    virtual void visitScope(AST::Scope*);
+    virtual void visitNamespace(AST::Namespace*);
+    virtual void visitClass(AST::Class*);
+
+private:
+    //. The indent depth
+    int m_indent;
+    //. The indent string
+    string m_indent_string;
+    //. Increases indent
+    void indent();
+    //. Decreases indent
+    void undent();
+
+};
+
+#endif // header guard
