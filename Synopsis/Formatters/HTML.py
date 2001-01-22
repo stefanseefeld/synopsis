995721db28bd67e26e76e411d9e5317917578a5a
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2001-01-22 17:06:15 +0000

    added copyright notice, and switched on logging

--- Synopsis/Formatters/HTML.py
+++ Synopsis/Formatters/HTML.py
@@ -1,3 +1,28 @@
+# $Id: HTML.py,v 1.48 2001/01/22 17:06:15 stefan Exp $
+#
+# This file is a part of Synopsis.
+# Copyright (C) 2000, 2001 Stephen Davies
+#
+# Synopsis is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Log: HTML.py,v $
+# Revision 1.48  2001/01/22 17:06:15  stefan
+# added copyright notice, and switched on logging
+#
+
 # HTML2 Formatter by Stephen Davies
 # Based on HTML Formatter by Stefan Seefeld
 #
--- Synopsis/Formatters/HTML_Simple.py
+++ Synopsis/Formatters/HTML_Simple.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python
+#
+#  $Id: HTML_Simple.py,v 1.5 2001/01/22 17:06:15 stefan Exp $
+#
+#  This file is a part of Synopsis.
+#  Copyright (C) 2000, 2001 Stefan Seefeld
+#
+#  Synopsis is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+#  02111-1307, USA.
+#
+# $Log: HTML_Simple.py,v $
+# Revision 1.5  2001/01/22 17:06:15  stefan
+# added copyright notice, and switched on logging
+#
+
 import sys, getopt, os, os.path, string
 from Synopsis.Core import Type, Util
 
