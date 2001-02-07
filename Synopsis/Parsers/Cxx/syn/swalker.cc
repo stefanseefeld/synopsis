22acb7cbac5db1278f732d975dffea67ec2aa92e
chalky <chalky@91adf076-48d4-0310-a9e2-da78070693fa>
2001-02-07 14:27:39 +0000

    Enums inside declarations swallow declaration comments.

--- Synopsis/Parsers/Cxx/syn/swalker.cc
+++ Synopsis/Parsers/Cxx/syn/swalker.cc
@@ -1,3 +1,30 @@
+// $Id: swalker.cc,v 1.16 2001/02/07 14:27:39 chalky Exp $
+//
+// This file is a part of Synopsis.
+// Copyright (C) 2000, 2001 Stephen Davies
+// Copyright (C) 2000, 2001 Stefan Seefeld
+//
+// Synopsis is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
+//
+// $Log: swalker.cc,v $
+// Revision 1.16  2001/02/07 14:27:39  chalky
+// Enums inside declarations swallow declaration comments.
+//
+//
+
 // File: swalker.cc
 //
 // SWalker class
@@ -347,6 +374,7 @@ Ptree* SWalker::TranslateDeclaration(Ptree* def)
 	if (!decls->IsLeaf())	// if it is not ";"
 	    TranslateDeclarators(decls);
     }
+    m_declaration = NULL;
     return 0;
 }
 
@@ -652,6 +680,11 @@ Ptree *SWalker::TranslateEnumSpec(Ptree *spec)
     // Create AST.Enum object
     AST::Enum* theEnum = m_builder->addEnum(enum_lineno,name,enumerators);
     addComments(theEnum, m_declaration);
+    if (m_declaration) {
+	// Enum declared inside declaration. Comments for the declaration
+	// belong to the enum. This is policy. #TODO review policy
+	m_declaration->SetComments(nil);
+    }
     return 0;
 }
 
