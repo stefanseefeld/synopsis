0bea72adf30b77db09140ba439c3817ec504c86f
chalky <chalky@91adf076-48d4-0310-a9e2-da78070693fa>
2001-03-29 14:11:33 +0000

    Refactoring for use by RefManual's own derived module

--- Synopsis/Formatters/HTML/Views/ModuleListingJS.py
+++ Synopsis/Formatters/HTML/Views/ModuleListingJS.py
@@ -1,4 +1,4 @@
-# $Id: ModuleListingJS.py,v 1.3 2001/02/06 05:12:46 chalky Exp $
+# $Id: ModuleListingJS.py,v 1.4 2001/03/29 14:11:33 chalky Exp $
 #
 # This file is a part of Synopsis.
 # Copyright (C) 2000, 2001 Stephen Davies
@@ -20,6 +20,9 @@
 # 02111-1307, USA.
 #
 # $Log: ModuleListingJS.py,v $
+# Revision 1.4  2001/03/29 14:11:33  chalky
+# Refactoring for use by RefManual's own derived module
+#
 # Revision 1.3  2001/02/06 05:12:46  chalky
 # Added JSTree class and FileTreeJS and modified ModuleListingJS to use JSTree
 #
@@ -52,10 +55,15 @@ class ModuleListingJS(JSTree.JSTree):
     expand/collapse sections of the tree!"""
     def __init__(self, manager):
 	JSTree.JSTree.__init__(self, manager)
-	self.__filename = config.files.nameOfSpecial('module_listing')
-	link = href(self.__filename, 'Modules', target="contents")
-	manager.addRootPage('Modules', link, 2)
-	config.set_contents_page(self.__filename)
+	self._init_page()
+
+    def _init_page(self):
+	"Sets _filename and registers the page with the manager"
+	self._filename = config.files.nameOfSpecial('module_listing')
+	link = href(self._filename, 'Modules', target="contents")
+	self.manager.addRootPage('Modules', link, 2)
+	config.set_contents_page(self._filename)
+	self._link_target = 'index'
 
     def process(self, start):
 	"""Create a page with an index of all modules"""
@@ -63,7 +71,7 @@ class ModuleListingJS(JSTree.JSTree):
 	share = os.path.split(AST.__file__)[0]+"/../share" #hack..
 	self.js_init(share+'/syn-down.png', share+'/syn-right.png', 'tree_%s.png', 0)
 	# Creare the file
-	self.startFile(self.__filename, "Module Index")
+	self.startFile(self._filename, "Module Index")
 	self.write(self.manager.formatRoots('Modules', 2))
 	self.write('<hr>')
 	self.indexModule(start, start.name())
@@ -71,6 +79,8 @@ class ModuleListingJS(JSTree.JSTree):
 
     def _child_filter(self, child):
 	return isinstance(child, AST.Module)
+    def _link_href(self, ns):
+	return config.files.nameOfModuleIndex(ns.name())
     def indexModule(self, ns, rel_scope):
 	"Write a link for this module and recursively visit child modules."
 	my_scope = ns.name()
@@ -82,8 +92,8 @@ class ModuleListingJS(JSTree.JSTree):
 	# Print link to this module
 	button = len(children) and self.formatButton() or ""
 	name = Util.ccolonName(my_scope, rel_scope) or "Global Namespace"
-	link = config.files.nameOfModuleIndex(ns.name())
-	self.write(button + href(link, name, target='index') + '<br>')
+	link = self._link_href(ns)
+	self.write(button + href(link, name, target=self._link_target) + '<br>')
 	# Add children
 	if not len(children): return
 	self.startSection()
