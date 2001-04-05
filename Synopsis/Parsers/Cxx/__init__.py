b67947ff340e22ae5f6f5ec5f35a6d78501604b5
chalky <chalky@91adf076-48d4-0310-a9e2-da78070693fa>
2001-04-05 14:04:25 +0000

    Add comment

--- Synopsis/Parsers/Cxx/__init__.py
+++ Synopsis/Parsers/Cxx/__init__.py
@@ -1,4 +1,9 @@
-"""Parser for C++ using OpenC++ for low-level parsing."""
+"""Parser for C++ using OpenC++ for low-level parsing.
+This parser is written entirely in C++, and compiled into shared libraries for
+use by python.
+@see C++/Synopsis
+@see C++/SWalker
+"""
 #
 # configure for the parser you want here...
 #
