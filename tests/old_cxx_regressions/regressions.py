6284948ce4da98fb702951c86f602ce0ff9323d0
chalky <chalky@91adf076-48d4-0310-a9e2-da78070693fa>
2002-10-11 05:57:44 +0000

    Added regression test to check comment proximity (comment next to declaration)

--- tests/old_cxx_regressions/regressions.py
+++ tests/old_cxx_regressions/regressions.py
@@ -29,6 +29,33 @@ class Comments (Regression, Test):
  */
 """
 
+class CommentProximity (Regression, Test):
+  test = """// Comment at start
+
+int test1;
+
+// Test2: This should have a comment, unlike test1
+// It should also have two lines
+int test2;
+
+/* A comment by itself
+ * Over multiple lines. */
+
+int test3;
+
+/* This comment should be ignored.
+ */
+/* Test4: Should have a comment
+ * */
+int test4;
+
+/* Test5: This should have a comment, unlike test 3
+ * Which had a gap.
+ */
+int test5;
+//< This should still be tail-appended!
+"""
+
 class IfTest (Regression, Test):
   test = """// If test
 void func() {
