d1dea91c2dfc146692a1aab8f36da16ddc7e549c
chalky <chalky@91adf076-48d4-0310-a9e2-da78070693fa>
2002-10-20 03:12:24 +0000

    Add test for concat operator

--- tests/old_cxx_regressions/regressions.py
+++ tests/old_cxx_regressions/regressions.py
@@ -392,4 +392,16 @@ public:
 };
 """
 
+class ConcatTest (Regression, Test):
+  test = """
+// According to spec, the ## operator must produce a valid PP token.
+// Unfortunately, no compilers known to man enforce this rule except UCPP, so
+// this test is to make sure it is 'fixed' to ignore the error.
+#define CAT(a,b) a ## b
+
+class Foo {};
+void CAT(operator,+) (const Foo&, const Foo&) {}
+void operator CAT(+,=) (const Foo&, const Foo&) {}
+"""
+
 # vim: set et sts=2 ts=8 sw=2:
