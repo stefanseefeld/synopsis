7b9caa70d3abe6eada8d053fe2f1460a75aaa142
chalky <chalky@91adf076-48d4-0310-a9e2-da78070693fa>
2002-10-27 12:08:47 +0000

    Add a test for funcptrs in parameters

--- tests/old_cxx_regressions/regressions.py
+++ tests/old_cxx_regressions/regressions.py
@@ -450,4 +450,26 @@ template <class R>
 struct function<R (int, int)> { };
 """
 
+class FuncPtrTest (Regression, Test):
+  test = """
+// Tests function pointers
+
+typedef void PyObject;
+
+// A function which takes a func ptr as a parameter
+void insert(void* (*convert)(PyObject*), bool yesno);
+
+// A function which takes a func ptr as a parameter
+void insert(void* (*convert)(PyObject*, int), bool yesno);
+
+#if 0
+// A function which returns a func ptr
+void* (*insert2)(int)(PyObject*);
+
+int main() {
+  (void)insert2(1);
+}
+#endif
+"""
+
 # vim: set et sts=2 ts=8 sw=2:
