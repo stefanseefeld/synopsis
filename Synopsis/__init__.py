4b98e44e996da065f058de5fb71e27cf7fdc995d
Stefan Seefeld <stefan@seefeld.name>
2003-09-10 21:42:08 +0000

    update version (and trigger a new cvs backup on sf.net)

--- Synopsis/__init__.py
+++ Synopsis/__init__.py
@@ -4,4 +4,4 @@ This package contains the Synopsis modules loaded at runtime. These modules
 are organised into four subpackages: Core, Parser, Linker and Formatter. There
 is also the Config module, which contains all the default configuration
 settings."""
-__version__ = "0.3"
+__version__ = "0.5"
