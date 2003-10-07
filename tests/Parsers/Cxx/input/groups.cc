5be2df6948492b4f8437713717be9afaadb93764
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2003-10-07 21:15:00 +0000

    adjust to new default grouping syntax

--- tests/Parsers/Cxx/input/groups.cc
+++ tests/Parsers/Cxx/input/groups.cc
@@ -1,11 +1,16 @@
-// { Group1
+// group documentation
+// @group first group { some comment
+// extending over multiple lines
 struct foo
 {
 };
 int test1;
 int test2;
-// }
-// { Group2
+// @group }
+// an unnamed group
+// @group {
+// test3 documentation
 int test3;
 int test4;
-// }
+// @group }
+
