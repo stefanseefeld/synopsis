35cce513f20884b285b593517dc048cec2e4abcf
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2003-11-05 17:21:50 +0000

    adapt to new Processor interface

--- Synopsis/Parsers/Python/Parser.py
+++ Synopsis/Parsers/Python/Parser.py
@@ -1,24 +1,23 @@
-# $Id: Parser.py,v 1.1 2003/10/30 05:54:36 stefan Exp $
+# $Id: Parser.py,v 1.2 2003/11/05 17:21:50 stefan Exp $
 #
 # Copyright (C) 2003 Stefan Seefeld
 # All rights reserved.
-# Licensed to the public under the terms of the GNU GPL (>= 2),
+# Licensed to the public under the terms of the GNU LGPL (>= 2),
 # see the file COPYING for details.
 #
 
 from Synopsis.Core.Processor import Processor
+from Synopsis.Core import AST
 from python import parse
 
 class Parser(Processor):
 
     basename=''
     
-    def process(self, **kwds):
+    def process(self, ast, **kwds):
         for file in kwds['input']:
-            print file
-            parse(file, 0, {}, None)
-#
-# $Log: Parser.py,v $
-# Revision 1.1  2003/10/30 05:54:36  stefan
-# add Processor base class
-#
+            ast.merge(parse(file, 0, {}, None))
+        output = kwds.get('output')
+        if output:
+            AST.save(output, ast)
+        return ast
