408063bdd80205528129d4ac466aa946ee16b990
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2003-11-11 02:57:57 +0000

    a major refactoring, part I

--- Synopsis/Processors/__init__.py
+++ Synopsis/Processors/__init__.py
@@ -4,5 +4,12 @@ There is much that can be done to an AST to manipulate it. This includes:
 merging multiple AST trees, mapping links to different languages, pruning the
 tree, mapping names to different scopes, and dealing with comment prefixes and
 groupings."""
-# this file is intentionally left empty
-# -stefan
+
+from Unduplicator import *
+from Stripper import *
+from NameMapper import *
+from Comments import *
+from XRefCompiler import *
+from EmptyNS import *
+from AccessRestrictor import *
+from LanguageMapper import *
