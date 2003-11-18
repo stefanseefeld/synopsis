6135d55a23a96fdd33d2bef32444baae0501b35d
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2003-11-18 16:44:11 +0000

    enhance the logic to find 'datadir'. It's still not perfect...

--- Synopsis/config.py
+++ Synopsis/config.py
@@ -2,8 +2,30 @@
 particular release / installation such as version, data
 paths, etc."""
 
-import os, os.path
+import os, os.path, sys
 
-datadir = os.path.abspath(os.path.join(__file__,
-                                       os.pardir, os.pardir,
-                                       'share', 'Synopsis'))
+if os.name == "posix":
+   datadir = os.path.abspath(os.path.join(__file__,
+                                          # lib / pythonx.y / site-packages / Synopsis / config.py
+                                          os.pardir, os.pardir, os.pardir, os.pardir, os.pardir,
+                                          'share', 'Synopsis'))
+elif os.name == "nt":
+   datadir = os.path.abspath(os.path.join(__file__,
+                                          # lib / site-packages / Synopsis / config.py
+                                          os.pardir, os.pardir, os.pardir, os.pardir,
+                                          'share', 'Synopsis'))
+
+elif os.name == "mac":
+   datadir = os.path.abspath(os.path.join(__file__,
+                                          # lib / site-packages / Synopsis / config.py
+                                          os.pardir, os.pardir, os.pardir, os.pardir,
+                                          'share', 'Synopsis'))
+
+if not os.path.exists(datadir) or not os.path.isdir(datadir):
+   # for now lets assume this is called from within the source tree
+   # we need a more robust way to deal with this whole issue, though
+   # --stefan
+
+   datadir = os.path.abspath(os.path.join(__file__,
+                                          os.pardir, os.pardir,
+                                          'share', 'Synopsis'))
