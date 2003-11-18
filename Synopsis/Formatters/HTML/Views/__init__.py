4b9f03cc0385cfe0a5808989ab22d67af9131d18
Stefan Seefeld <stefan@seefeld.name>
2003-11-18 07:28:01 +0000

    load all pages when importing 'Pages'

--- Synopsis/Formatters/HTML/Views/__init__.py
+++ Synopsis/Formatters/HTML/Views/__init__.py
@@ -1,4 +1,4 @@
-# $Id: __init__.py,v 1.1 2003/11/16 21:09:45 stefan Exp $
+# $Id: __init__.py,v 1.2 2003/11/18 07:28:01 stefan Exp $
 #
 # Copyright (C) 2000 Stephen Davies
 # Copyright (C) 2000 Stefan Seefeld
@@ -7,3 +7,18 @@
 # see the file COPYING for details.
 #
 
+from FramesIndex import FramesIndex
+from DirBrowse import DirBrowse
+from Scope import Scope
+from ModuleListing import ModuleListing
+from ModuleListingJS import ModuleListingJS
+from ModuleIndexer import ModuleIndexer
+from FileListing import FileListing
+from FileIndexer import FileIndexer
+from FileDetails import FileDetails
+from InheritanceTree import InheritanceTree
+from InheritanceGraph import InheritanceGraph
+from FileSource import FileSource
+from NameIndex import NameIndex
+from XRef import XRef
+from RawFile import RawFile
