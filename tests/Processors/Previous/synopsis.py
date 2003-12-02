0ed2478928d5e982c93eec174db8d4523922c6f4
Stefan Seefeld <stefan@seefeld.name>
2003-12-02 05:41:48 +0000

    more tests

---
+++ tests/Processors/Previous/synopsis.py
@@ -0,0 +1,10 @@
+from Synopsis.process import process
+from Synopsis.Processor import Composite
+from Synopsis.Parsers import Cxx
+from Synopsis.Processors.Comments import SSComments, Previous
+from Synopsis.Formatters import Dump
+
+process(parse = Composite(Cxx.Parser(),
+                          SSComments(),
+                          Previous(),
+                          Dump.Formatter(show_ids = False)))
