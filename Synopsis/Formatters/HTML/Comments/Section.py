72a1d0b69abd16ba5e5308016234d2f142f983dc
Stefan Seefeld <stefan@seefeld.name>
2003-12-04 21:04:28 +0000

    put comment formatters into their own package

---
+++ Synopsis/Formatters/HTML/Comments/Section.py
@@ -0,0 +1,32 @@
+# $Id: Section.py,v 1.1 2003/12/04 21:04:28 stefan Exp $
+#
+# Copyright (C) 2000 Stephen Davies
+# Copyright (C) 2000 Stefan Seefeld
+# All rights reserved.
+# Licensed to the public under the terms of the GNU LGPL (>= 2),
+# see the file COPYING for details.
+#
+
+from Formatter import Formatter
+import re
+
+class Section(Formatter):
+   """A test formatter"""
+
+   __re_break = '\n[ \t]*\n'
+
+   def __init__(self):
+
+      self.re_break = re.compile(Section.__re_break)
+
+   def format(self, page, decl, text):
+
+      if text is None: return text
+      para = '</p>\n<p>'
+      mo = self.re_break.search(text)
+      while mo:
+         start, end = mo.start(), mo.end()
+         text = text[:start] + para + text[end:]
+         end = start + len(para)
+         mo = self.re_break.search(text, end)
+      return '<p>%s</p>'%text
