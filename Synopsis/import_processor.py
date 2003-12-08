9c3eaf1a928adb898d11c7a7dcf6f510654b7cf9
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2003-12-08 18:24:20 +0000

    put parameter listing of processors into the tutorial appendix

---
+++ Synopsis/import_processor.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# $Id: import_processor.py,v 1.1 2003/12/08 18:24:20 stefan Exp $
+#
+# Copyright (C) 2003 Stefan Seefeld
+# All rights reserved.
+# Licensed to the public under the terms of the GNU LGPL (>= 2),
+# see the file COPYING for details.
+#
+
+def import_processor(name, verbose=False):
+   """Import a named processor and return it.
+   Throws ImportError on failure."""
+
+   
+   i = name.rfind('.')
+   if i == -1:
+      raise ImportError, '%s does not name a valid processor'%name
+
+   module, processor = name[:i], name.split('.')
+
+   mod = __import__(module)
+
+   for c in processor[1:]:
+      try:
+         mod = getattr(mod, c)
+      except AttributeError, msg:
+         raise ImportError, "Unable to find %s in:\n%s"%(c, repr(mod))
+
+   return mod
