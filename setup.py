552f67e14682fe3574be70b5a904e3d9d6259442
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2003-12-10 19:19:57 +0000

    cleanup; enable native win32 builds

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# $Id: setup.py,v 1.18 2003/12/09 21:46:29 stefan Exp $
+# $Id: setup.py,v 1.19 2003/12/10 19:19:57 stefan Exp $
 #
 # Setup script for synopsis
 #
@@ -45,11 +45,14 @@ py_packages = ["Synopsis",
 #"Synopsis.UI.Qt"] 
 
 
-ext_modules = [('Synopsis/Parsers/C', 'ctool' + module_ext),
-               ('Synopsis/Parsers/Cxx', 'occ' + module_ext),
+ext_modules = [('Synopsis/Parsers/Cxx', 'occ' + module_ext),
                ('Synopsis/Parsers/Cxx', 'link' + module_ext)]
 
-scripts = ['synopsis', 'synopsis-qt', 'compile-xref', 'search-xref']
+#('Synopsis/Parsers/C', 'ctool' + module_ext),
+
+scripts = ['synopsis']
+
+#, 'synopsis-qt', 'compile-xref', 'search-xref']
 
 data_files = []
 
