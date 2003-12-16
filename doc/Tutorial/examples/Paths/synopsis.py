3d134d03d4962bcc44fecb60ccfdfe58094ff445
Stefan Seefeld <stefan@seefeld.name>
2003-12-16 04:58:09 +0000

    more on the Tutorial

---
+++ doc/Tutorial/examples/Paths/synopsis.py
@@ -0,0 +1,44 @@
+from Synopsis.process import process
+from Synopsis.Processor import Processor, Parameter, Composite
+from Synopsis.Parsers import Cxx
+from Synopsis.Processors import Linker
+from Synopsis.Processors.Comments import SSComments
+from Synopsis.Processors.Comments import SSDComments
+from Synopsis.Processors.Comments import JavaComments
+from Synopsis.Processors.Comments import Previous
+from Synopsis.Processors.Comments import JavaTags
+from Synopsis.Processors.Comments import Grouper1
+from Synopsis.Formatters import HTML
+from Synopsis.Formatters.HTML import Comments
+from Synopsis.Formatters import Dot
+
+cxx_ssd = Composite(Cxx.Parser(), SSDComments())
+
+html = HTML.Formatter(comment_formatters = [Comments.QuoteHTML(),
+                                            Comments.Section(),
+                                            Comments.Javadoc()])
+
+class Joker(Processor):
+    
+   parameter = Parameter(':-)', 'a friendly parameter')
+
+   def process(self, ast, **keywords):
+      # override default parameter values
+      self.set_parameters(keywords)
+      # merge in ast from 'input' parameter if given
+      self.ast = self.merge_input(ast)
+
+      print 'this processor is harmless...', self.parameter
+      
+      # write to output (if given) and return ast
+      return self.output_and_return_ast()
+        
+
+process(cxx_ssd = cxx_ssd,
+        cxx_ss = Composite(Cxx.Parser(), SSComments()),
+        cxx_ssd_prev = Composite(Cxx.Parser(), SSDComments(), Previous()),
+        cxx_javadoc = Composite(Cxx.Parser(), JavaComments(), JavaTags()),
+        link = Linker(Grouper1()),
+        html = html,
+        dot = Dot.Formatter(),
+        joker = Joker(parameter = '(-;'))
