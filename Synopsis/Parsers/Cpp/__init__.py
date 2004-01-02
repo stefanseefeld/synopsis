cc35701f7c8df27c023ee096828c35e63bca0857
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2004-01-02 03:58:47 +0000

    first working version of standalone ucpp parser

---
+++ Synopsis/Parsers/Cpp/__init__.py
@@ -0,0 +1,9 @@
+# $Id: __init__.py,v 1.1 2004/01/02 03:58:47 stefan Exp $
+#
+# Copyright (C) 2003 Stefan Seefeld
+# All rights reserved.
+# Licensed to the public under the terms of the GNU LGPL (>= 2),
+# see the file COPYING for details.
+#
+
+from Parser import *
