275989b283f497e3c79812926a407770e0af2412
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2004-01-13 07:42:09 +0000

    bug fixes, and support for 'MacroCall' type

--- include/Synopsis/AST/SourceFile.hh
+++ include/Synopsis/AST/SourceFile.hh
@@ -1,4 +1,4 @@
-// $Id: SourceFile.hh,v 1.2 2004/01/11 19:46:29 stefan Exp $
+// $Id: SourceFile.hh,v 1.3 2004/01/13 07:42:09 stefan Exp $
 //
 // Copyright (C) 2004 Stefan Seefeld
 // All rights reserved.
@@ -26,6 +26,7 @@ public:
   bool is_main() { return narrow<bool>(Callable(attr("is_main")).call());}
   void is_main(bool flag) { Callable c(attr("set_is_main")); c.call(Tuple(flag));}
   List includes() { return List(Callable(attr("includes")).call());}
+  Dict macro_calls() { return Dict(Callable(attr("macro_calls")).call());}
 };
 
 class Include : public Object
@@ -36,6 +37,17 @@ public:
   bool is_macro() const { return narrow<bool>(Callable(attr("is_macro")).call());}
   bool is_next() const { return narrow<bool>(Callable(attr("is_next")).call());}
   void assert_type() throw(TypeError) { Object::assert_type("Synopsis.AST", "Include");}
+};
+
+class MacroCall : public Object
+{
+public:
+  MacroCall(const Object &o) throw(TypeError) : Object(o) { assert_type();}
+  void assert_type() throw(TypeError) { Object::assert_type("Synopsis.AST", "MacroCall");}
+  std::string name() { return narrow<std::string>(attr("name"));}
+  int start() { return narrow<int>(attr("start"));}
+  int end() { return narrow<int>(attr("end"));}
+  int diff() { return narrow<int>(attr("diff"));}
 };
 
 }
