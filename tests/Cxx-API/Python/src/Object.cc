c558111b5d6775e28f8d5e07baf06fc64550f407
Stefan Seefeld <stefan@seefeld.name>
2004-01-14 04:04:54 +0000

    add List iterator

--- tests/Cxx-API/Python/src/Object.cc
+++ tests/Cxx-API/Python/src/Object.cc
@@ -10,13 +10,13 @@ void test1()
   Object object;
   Object character('q');
   std::cout << Object::narrow<char>(character) << std::endl;
-  std::cout << Object::narrow<const char *>(character.str()) << std::endl;
+  std::cout << Object::narrow<std::string>(character.str()) << std::endl;
   Object string("hello world");
-  std::cout << Object::narrow<const char *>(string) << std::endl;
-  std::cout << Object::narrow<const char *>(string.str()) << std::endl;
+  std::cout << Object::narrow<std::string>(string) << std::endl;
+  std::cout << Object::narrow<std::string>(string.str()) << std::endl;
   Object integer(42);
   std::cout << Object::narrow<long>(integer) << std::endl;
-  std::cout << Object::narrow<const char *>(integer.str()) << std::endl;
+  std::cout << Object::narrow<std::string>(integer.str()) << std::endl;
   try { std::cout << Object::narrow<long>(string) << std::endl;}
   catch (Object::TypeError &e) { std::cout << e.what() << std::endl;}
 }
