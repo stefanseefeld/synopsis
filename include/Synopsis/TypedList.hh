80fde557963dcc705ee4108d2aeba495ad9fc218
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2004-03-17 04:18:41 +0000

    add convenience constructors

--- include/Synopsis/TypedList.hh
+++ include/Synopsis/TypedList.hh
@@ -22,11 +22,47 @@ class TypedList : public List
 public:
   TypedList(size_t i = 0) : List(i) {}
   TypedList(Object o) : List(o) {} // should we typecheck here all items ?
+
+  TypedList(const T &);
+  TypedList(const T &, const T &);
+  TypedList(const T &, const T &, const T &);
+  TypedList(const T &, const T &, const T &, const T &);
+
   void set(int i, const T &s) { List::set(i, s);}
   T get(int i) const { return narrow<T>(List::get(i));}
   void append(const T &s) { List::append(s);}
   void insert(int i, const T &s) { List::insert(i, s);}
 };
+
+template <typename T>
+inline TypedList<T>::TypedList(const T &t1)
+{
+  append(t1);
+}
+
+template <typename T>
+inline TypedList<T>::TypedList(const T &t1, const T &t2)
+{
+  append(t1);
+  append(t2);
+}
+
+template <typename T>
+inline TypedList<T>::TypedList(const T &t1, const T &t2, const T &t3)
+{
+  append(t1);
+  append(t2);
+  append(t3);
+}
+
+template <typename T>
+inline TypedList<T>::TypedList(const T &t1, const T &t2, const T &t3, const T &t4)
+{
+  append(t1);
+  append(t2);
+  append(t3);
+  append(t4);
+}
 
 }
 
