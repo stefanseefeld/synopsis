b5699b896869edbf836f2acbedb0936a5e7b7947
Stefan Seefeld <stefan@seefeld.name>
2004-03-19 21:58:53 +0000

    make option parsing more flexible

--- Synopsis/getoptions.py
+++ Synopsis/getoptions.py
@@ -10,35 +10,47 @@ from __future__ import generators
 
 import sys
 
-def getoptions(args):
-   """provide an iterator over the options in args.
-   All found options are stripped, such that args will
-   contain the remainder, i.e. non-option arguments.
-   The required format is '--option[=[arg]]' or 'option=arg'.
+def parse_option(arg):
+   """The required format is '--option[=[arg]]' or 'option=arg'.
    In the former case the optional argument is interpreted as
    a string (only '--option' sets the value to True, '--option='
    sets it to the empty string), in the latter case the argument
    is evaluated as a python expression.
+   Returns (None, None) for non-option argument"""
+   
+   if arg.find('=') == -1 and not arg.startswith('--'):
+      return None, None # we are done
+   attribute = arg.split('=', 1)
+   if len(attribute) == 2:
+      name, value = attribute
+      if name.startswith('--'):
+         name = name[2:] # value is a string
+      else:
+         try:
+            value = eval(value) # it's a python expression
+         except:
+            sys.stderr.write("""an error occured trying to evaluate the value of \'%s\' (\'%s\')
+to pass this as a string, please use %s="'%s'" \n"""%(name, value, name, value))
+            sys.exit(-1)
+   else:
+      name, value = attribute[0][2:], True # flag the attribute as 'set'
+
+   return name, value
+
+def get_options(args, parse_arg = parse_option):
+   """provide an iterator over the options in args.
+   All found options are stripped, such that args will
+   contain the remainder, i.e. non-option arguments.
+   Pass each argument to the parse_option function to
+   extract the (name,value) pair. Returns as soon as
+   the first non-option argument was detected.
    """
 
    while args:
-      arg = args[0]
-      if arg.find('=') == -1 and not arg.startswith('--'):
-         return # we are done
-      attribute = arg.split('=', 1)
-      if len(attribute) == 2:
-         name, value = attribute
-         if name.startswith('--'):
-            name = name[2:] # value is a string
-         else:
-            try:
-               value = eval(value) # it's a python expression
-            except:
-               sys.stderr.write("""an error occured trying to evaluate the value of \'%s\' (\'%s\')
-to pass this as a string, please use %s="'%s'" \n"""%(name, value, name, value))
-               sys.exit(-1)
+      name, value = parse_arg(args[0])
+      if name:
+         args[:] = args[1:]
+         yield name, value
       else:
-         name, value = attribute[0][2:], True # flag the attribute as 'set'
-
-      args[:] = args[1:]
-      yield name, value
+         return
+      
