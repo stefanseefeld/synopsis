b6709afa05f422314c411ffc5cd1aa1781bc0847
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2004-06-15 20:54:06 +0000

    new temp libSynopsis.a helper library

---
+++ Cxx-API/include/Synopsis/AST/SourceFile.hh
@@ -0,0 +1,44 @@
+//
+// Copyright (C) 2004 Stefan Seefeld
+// All rights reserved.
+// Licensed to the public under the terms of the GNU LGPL (>= 2),
+// see the file COPYING for details.
+//
+
+#ifndef _Synopsis_AST_SourceFile_hh
+#define _Synopsis_AST_SourceFile_hh
+
+#include <Synopsis/Object.hh>
+#include <Synopsis/AST/Declaration.hh>
+
+namespace Synopsis
+{
+namespace AST
+{
+
+class Include : public Object
+{
+public:
+  Include(const Object &o) throw(TypeError) : Object(o) { assert_type();}
+  SourceFile target() const { return narrow<SourceFile>(attr("target")());}
+  std::string name() const { return narrow<std::string>(attr("name")());}
+  bool is_macro() const { return narrow<bool>(attr("is_macro")());}
+  bool is_next() const { return narrow<bool>(attr("is_next")());}
+  void assert_type() throw(TypeError) { Object::assert_type("Synopsis.AST", "Include");}
+};
+
+class MacroCall : public Object
+{
+public:
+  MacroCall(const Object &o) throw(TypeError) : Object(o) { assert_type();}
+  void assert_type() throw(TypeError) { Object::assert_type("Synopsis.AST", "MacroCall");}
+  std::string name() { return narrow<std::string>(attr("name"));}
+  int start() { return narrow<int>(attr("start"));}
+  int end() { return narrow<int>(attr("end"));}
+  int diff() { return narrow<int>(attr("diff"));}
+};
+
+}
+}
+
+#endif
---
+++ Cxx-API/src/Path-posix.cc
@@ -0,0 +1,84 @@
+//
+// Copyright (C) 2004 Stefan Seefeld
+// All rights reserved.
+// Licensed to the public under the terms of the GNU LGPL (>= 2),
+// see the file COPYING for details.
+//
+
+#include <Synopsis/Path.hh>
+#include <vector>
+#include <stdexcept>
+#include <cerrno>
+#include <unistd.h>
+
+using namespace Synopsis;
+
+std::string Path::cwd()
+{
+  static std::string path;
+  if (path.empty())
+    for (long path_max = 32;; path_max *= 2)
+    {
+      char *buf = new char[path_max];
+      if (::getcwd(buf, path_max) == 0)
+      {
+	if (errno != ERANGE)
+	{
+	  delete [] buf;
+	  throw std::runtime_error(strerror(errno));
+	}
+      }
+      else
+      {
+	path = buf;
+	delete [] buf;
+	return path;
+      }
+      delete [] buf;
+    }
+  return path;
+}
+
+std::string Path::normalize(const std::string &filename)
+{
+  std::string value = filename;
+  char separator = '/';
+  const char *pat1 = "/./";
+  const char *pat2 = "/../";
+  if (value[0] != separator)
+    value.insert(0, Path::cwd() + separator);
+
+  // nothing to do...
+  if (value.find(pat1) == std::string::npos &&
+      value.find(pat2) == std::string::npos) return value;
+  
+  // for the rest we'll operate on a decomposition of the filename...
+  typedef std::vector<std::string> Components;
+  Components components;
+
+  std::string::size_type b = 0;
+  while (b < value.size())
+  {
+    std::string::size_type e = value.find(separator, b);
+    components.push_back(value.substr(b, e-b));
+    b = e == std::string::npos ? std::string::npos : e + 1;
+  }
+
+  // remove all '.' and '' components
+  components.erase(std::remove(components.begin(), components.end(), "."), components.end());
+  components.erase(std::remove(components.begin(), components.end(), ""), components.end());
+  // now collapse '..' components with the preceding one
+  while (true)
+  {
+    Components::iterator i = std::find(components.begin(), components.end(), "..");
+    if (i == components.end()) break;
+    if (i == components.begin()) throw std::invalid_argument("invalid path");
+    components.erase(i - 1, i + 1); // remove two components
+  }
+
+  // now rebuild the path as a string
+  std::string retn = '/' + components.front();
+  for (Components::iterator i = components.begin() + 1; i != components.end(); ++i)
+    retn += '/' + *i;
+  return retn;
+}
