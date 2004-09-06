2f23459a22b6b0d6e81a1025acdf5f42324cfb65
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2004-09-06 21:35:38 +0000

    optionally print typeid in PTree::Display

--- Synopsis/Parsers/Cxx/display-ptree.cc
+++ Synopsis/Parsers/Cxx/display-ptree.cc
@@ -8,28 +8,52 @@
 #include <PTree/Display.hh>
 #include <fstream>
 
+int usage(const char *command)
+{
+  std::cerr << "Usage: " << command << " [-t] [-r] <input>" << std::endl;
+  return -1;
+}
+
 int main(int argc, char **argv)
 {
-  if (argc != 2 && (argc != 3 || argv[1] != std::string("-t")))
+  bool translate = false;
+  bool typeinfo = false;
+  const char *input = argv[1];
+  switch (argc)
   {
-    std::cerr << "Usage: " << argv[0] << " [-t] <input>" << std::endl;
-    exit(-1);
+    case 2: break;
+    case 3:
+      if (argv[1] == std::string("-t")) translate = true;
+      else if (argv[1] == std::string("-r")) typeinfo = true;
+      else return usage(argv[0]);
+      input = argv[2];
+      break;
+    case 4:
+      if (!(argv[1] == std::string("-t") && argv[2] == std::string("-r")) &&
+	  !(argv[1] == std::string("-r") && argv[2] == std::string("-t")))
+	return usage(argv[0]);
+      translate = true;
+      typeinfo = true;
+      input = argv[3];
+      break;
+    default: return usage(argv[0]);
   }
   try
   {
-    if (argc == 3) // with translation
-    {
-      Class::do_init_static();
-      Metaclass::do_init_static();
-      Environment::do_init_static();
-      PTree::Encoding::do_init_static();
+    Class::do_init_static();
+    Metaclass::do_init_static();
+    Environment::do_init_static();
+    PTree::Encoding::do_init_static();
 
-      std::ifstream ifs(argv[2]);
-      Buffer buffer(ifs.rdbuf());
-      Lexer lexer(&buffer);
-      Parser parser(&lexer);
+    std::ifstream ifs(input);
+    Buffer buffer(ifs.rdbuf());
+    Lexer lexer(&buffer);
+    Parser parser(&lexer);
+
+    if (translate)
+    {
       ClassWalker translator(&parser);
-      PTree::Display display(std::cout, true);
+      PTree::Display display(std::cout, true, typeinfo);
       PTree::Node *node;
       while (parser.parse(node))
       {
@@ -37,13 +61,9 @@ int main(int argc, char **argv)
 	display.display(result);
       }
     }
-    else // no translation
+    else
     {
-      std::ifstream ifs(argv[1]);
-      Buffer buffer(ifs.rdbuf());
-      Lexer lexer(&buffer);
-      Parser parser(&lexer);
-      PTree::Display display(std::cout, true);
+      PTree::Display display(std::cout, true, typeinfo);
       PTree::Node *node;
       while (parser.parse(node)) display.display(node);
     }
