6bc8e0f457930f5e00a340f7d2016d9a76062b70
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2005-01-24 19:41:16 +0000

    get some basic support for symbol lookup testing

--- tests/OpenCxx/src/SymbolLookup.cc
+++ tests/OpenCxx/src/SymbolLookup.cc
@@ -15,14 +15,45 @@ using namespace Synopsis;
 class SymbolFinder : public SymbolLookup::Visitor
 {
 public:
-  SymbolFinder(SymbolLookup::Table &table, std::ostream &os)
-    : SymbolLookup::Visitor(table), my_os(os) {}
+  SymbolFinder(Buffer const &buffer, SymbolLookup::Table &table, std::ostream &os)
+    : SymbolLookup::Visitor(table), my_buffer(buffer), my_os(os) {}
   void find(PTree::Node *node) { node->accept(this);}
 private:
   virtual void visit(PTree::Declarator *decl)
   {
+    PTree::Encoding name = decl->encoded_name();
+    std::cout << "Declarator : " << name << std::endl;
+    // now find it.
+    SymbolLookup::SymbolSet symbols = table().lookup(name);
+    if (!symbols.empty())
+    {
+      std::string filename;
+      SymbolLookup::Symbol const *first = *symbols.begin();
+      unsigned long line_number = my_buffer.origin(first->ptree()->begin(), filename);
+      std::cout << "declared at line " << line_number << " in " << filename << std::endl;
+    }
+    else
+      std::cout << "undeclared ! " << std::endl;
   }
 
+  virtual void visit(PTree::Name *n)
+  {
+    PTree::Encoding name = n->encoded_name();
+    std::cout << "Name : " << name << std::endl;
+    // now find it.
+    SymbolLookup::SymbolSet symbols = table().lookup(name);
+    if (!symbols.empty())
+    {
+      std::string filename;
+      SymbolLookup::Symbol const *first = *symbols.begin();
+      unsigned long line_number = my_buffer.origin(first->ptree()->begin(), filename);
+      std::cout << "declared at line " << line_number << " in " << filename << std::endl;
+    }
+    else
+      std::cout << "undeclared ! " << std::endl;
+  }
+  
+  Buffer const &my_buffer;
   std::ostream &my_os;
 };
 
@@ -37,12 +68,12 @@ int main(int argc, char **argv)
   {
     std::ofstream ofs(argv[1]);
     std::ifstream ifs(argv[2]);
-    Buffer buffer(ifs.rdbuf());
+    Buffer buffer(ifs.rdbuf(), argv[2]);
     Lexer lexer(&buffer);
     SymbolLookup::Table symbols;
     Parser parser(lexer, symbols);
     PTree::Node *node = parser.parse();
-    SymbolFinder finder(symbols, ofs);
+    SymbolFinder finder(buffer, symbols, ofs);
     finder.find(node);
   }
   catch (const std::exception &e)
