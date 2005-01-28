a317b87ce5174aa4ffaa44d10b2912016397a263
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2005-01-28 17:41:37 +0000

    add option to generate dot output

--- Synopsis/Parsers/Cxx/display-ptree.cc
+++ Synopsis/Parsers/Cxx/display-ptree.cc
@@ -14,20 +14,70 @@ using Synopsis::Trace;
 
 int usage(const char *command)
 {
-  std::cerr << "Usage: " << command << " [-d] [-t] [-r] <input>" << std::endl;
+  std::cerr << "Usage: " << command << " [-g <filename>] [-d] [-t] [-r] <input>" << std::endl;
   return -1;
 }
+
+class DotFileGenerator : public PTree::Visitor
+{
+public:
+  DotFileGenerator(std::string const &filename) : my_os(filename.c_str()) {}
+  void write(PTree::Node *ptree)
+  {
+    my_os << "digraph PTree\n{\n"
+	  << "node[fillcolor=\"#ffffcc\", pencolor=\"#424242\" style=\"filled\"];\n";
+    ptree->accept(this);
+    my_os << '}' << std::endl;
+  }
+private:
+  virtual void visit(PTree::Atom *a)
+  {
+    my_os << (long)a 
+	  << " [label=\"" << std::string(a->position(), a->length())
+	  << "\" fillcolor=\"#ffcccc\"];\n";
+  }
+  virtual void visit(PTree::List *l)
+  {
+    my_os << (long)l 
+	  << " [label=\"" << typeid(*l).name() << "\"];\n";
+    if (l->car())
+    {
+      l->car()->accept(this);
+      my_os << (long)l << "->" 
+	    << (long)l->car() << ';' << std::endl;
+    }
+    if (l->cdr())
+    {
+      l->cdr()->accept(this);
+      my_os << (long)l << "->" 
+	    << (long)l->cdr() << ';' << std::endl;
+    }
+  }
+
+  std::ofstream my_os;
+};
 
 int main(int argc, char **argv)
 {
   bool translate = false;
   bool typeinfo = false;
   bool debug = false;
+  std::string dotfile;
   const char *input = argv[1];
   if (argc == 1) return usage(argv[0]);
   for (int i = 1; i < argc - 1; ++i)
   {
-    if (argv[i] == std::string("-t")) translate = true;
+    if (argv[i] == std::string("-g"))
+    {
+      // make sure an argument is provided
+      if (++i == argc - 1 || argv[i][0] == '-')
+      {
+	usage(argv[0]);
+	return -1;
+      }
+      dotfile = argv[i];
+    }
+    else if (argv[i] == std::string("-t")) translate = true;
     else if (argv[i] == std::string("-r")) typeinfo = true;
     else if (argv[i] == std::string("-d")) debug = true;
     else return usage(argv[0]);
@@ -56,12 +106,16 @@ int main(int argc, char **argv)
     if (translate)
     {
       ClassWalker translator(&buffer);
-      PTree::Node *result = translator.translate(node);
-      PTree::display(result, std::cout, true, typeinfo);
+      node = translator.translate(node);
+    }
+    if (dotfile.empty())
+    {
+      PTree::display(node, std::cout, true, typeinfo);
     }
     else
     {
-      PTree::display(node, std::cout, true, typeinfo);
+      DotFileGenerator dot(dotfile);
+      dot.write(node);
     }
   }
   catch (const std::exception &e)
