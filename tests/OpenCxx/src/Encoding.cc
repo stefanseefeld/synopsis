b7692e2c8e550447399bd1e716c7e45063a2e0b3
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2005-02-07 06:01:14 +0000

    * Simplify resource setup for OpenCxx tests. * Add '-d' option to all OpenCxx tests. * Do some more work on overload resolution.

--- tests/OpenCxx/src/Encoding.cc
+++ tests/OpenCxx/src/Encoding.cc
@@ -102,16 +102,29 @@ private:
 
 int main(int argc, char **argv)
 {
-  if (argc != 3)
+  if (argc < 3)
   {
-    std::cerr << "Usage: " << argv[0] << " <output> <input>" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " [-d] <output> <input>" << std::endl;
     exit(-1);
   }
   try
   {
-    std::ofstream ofs(argv[1]);
-    std::ifstream ifs(argv[2]);
-    Buffer buffer(ifs.rdbuf(), argv[2]);
+    std::string output;
+    std::string input;
+    if (argv[1] == std::string("-d"))
+    {
+      Trace::enable_debug();
+      output = argv[2];
+      input = argv[3];
+    }
+    else
+    {
+      output = argv[1];
+      input = argv[2];
+    }
+    std::ofstream ofs(output.c_str());
+    std::ifstream ifs(input.c_str());
+    Buffer buffer(ifs.rdbuf(), input);
     Lexer lexer(&buffer);
     SymbolLookup::Table symbols;
     Parser parser(lexer, symbols);
@@ -119,7 +132,7 @@ int main(int argc, char **argv)
     PTree::Node *node = parser.parse();
     finder.find(node);
   }
-  catch (const std::exception &e)
+  catch (std::exception const &e)
   {
     std::cerr << "Caught exception : " << e.what() << std::endl;
   }
--- tests/OpenCxx/src/Parser.cc
+++ tests/OpenCxx/src/Parser.cc
@@ -11,17 +11,37 @@ using namespace Synopsis;
 
 int main(int argc, char **argv)
 {
-  if (argc != 3)
+  if (argc < 3)
   {
-    std::cerr << "Usage: " << argv[0] << " <output> <input>" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " [-d] <output> <input>" << std::endl;
     exit(-1);
   }
-  std::ofstream ofs(argv[1]);
-  std::ifstream ifs(argv[2]);
-  Buffer buffer(ifs.rdbuf());
-  Lexer lexer(&buffer);
-  SymbolLookup::Table symbols;
-  Parser parser(lexer, symbols);
-  PTree::Node *node = parser.parse();
-  PTree::display(node, ofs, true);
+  try
+  {
+    std::string output;
+    std::string input;
+    if (argv[1] == std::string("-d"))
+    {
+      Trace::enable_debug();
+      output = argv[2];
+      input = argv[3];
+    }
+    else
+    {
+      output = argv[1];
+      input = argv[2];
+    }
+    std::ofstream ofs(output.c_str());
+    std::ifstream ifs(input.c_str());
+    Buffer buffer(ifs.rdbuf(), input);
+    Lexer lexer(&buffer);
+    SymbolLookup::Table symbols;
+    Parser parser(lexer, symbols);
+    PTree::Node *node = parser.parse();
+    PTree::display(node, ofs, true);
+  }
+  catch (std::exception const &e)
+  {
+    std::cerr << "Caught exception : " << e.what() << std::endl;
+  }
 }
