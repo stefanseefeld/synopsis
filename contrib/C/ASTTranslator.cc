b67f6af6207e8ba4a3bed691652e42e0ad30af10
Stefan Seefeld <stefan@seefeld.name>
2005-03-07 06:04:28 +0000

    Enhance C parser: * support enums * support functions * support variables

--- contrib/C/ASTTranslator.cc
+++ contrib/C/ASTTranslator.cc
@@ -34,29 +34,139 @@ void ASTTranslator::visit(PTree::List *node)
   if (node->cdr()) node->cdr()->accept(this);
 }
 
-void ASTTranslator::visit(PTree::Declarator *decl)
+void ASTTranslator::visit(PTree::Declarator *declarator)
 {
   Trace trace("ASTTranslator::visit(PTree::Declarator *)");
-  PTree::Encoding name = decl->encoded_name();
-  PTree::Encoding type = decl->encoded_type();
+  PTree::Encoding name = declarator->encoded_name();
+  PTree::Encoding type = declarator->encoded_type();
   if (type.is_function())
   {
-    // only declare once...
-    trace << "declare function " << name;
- //    tt.translate(type);
-    AST::Type return_type;
+    AST::TypeList parameter_types;
+    AST::Type return_type = my_types.lookup_function_types(type, parameter_types);
     AST::Function::Parameters parameters;
-    AST::ScopedName n(std::string(name.begin() + 1, name.end()));
-//     AST::Function func = my_kit.create_function(this->file, -1, "C", "function",
-// 						AST::Modifiers(),
-// 						return_type,
-// 						name,
-// 						this->symbol);
+    PTree::Node *p = PTree::rest(declarator);
+    while (p && p->car() && *p->car() != '(') p = PTree::rest(p);
+    translate_parameters(PTree::second(p), parameter_types, parameters);
+
+    size_t length = (name.front() - 0x80);
+    AST::ScopedName qname(std::string(name.begin() + 1, name.begin() + 1 + length));
+    AST::Modifiers modifiers;
+    AST::Function function = my_ast_kit.create_function(my_file, my_lineno,
+							"C",
+							"function",
+							modifiers,
+							return_type,
+							qname,
+							qname.get(0));
+    function.parameters().extend(parameters);
+    add_comments(function, declarator->get_comments());
+    my_ast.declarations().append(function);
   }
   else
   {
-    trace << "declare variable " << name;
+    AST::Type t = my_types.lookup(type);
+    size_t length = (name.front() - 0x80);
+    AST::ScopedName qname(std::string(name.begin() + 1, name.begin() + 1 + length));
+
+    std::string vtype;// = my_builder->scope()->type();
+    if (vtype == "class" || vtype == "struct" || vtype == "union")
+      vtype = "data member";
+    else
+    {
+      if (vtype == "function")
+	vtype = "local";
+      vtype += " variable";
+    }
+    AST::Variable variable = my_ast_kit.create_variable(my_file, my_lineno, "C",
+							vtype, qname, t, false);
+    add_comments(variable, declarator->get_comments());
+    my_ast.declarations().append(variable);
   }
+}
+
+void ASTTranslator::visit(PTree::ClassSpec *class_spec)
+{
+  Trace trace("ASTTranslator::visit(PTree::ClassSpec *)");
+  update_position(class_spec);
+  size_t size = PTree::length(class_spec);
+  if (size == 2) // forward declaration
+  {
+    // [ class|struct <name> ]
+
+    std::string type = PTree::reify(PTree::first(class_spec));
+    std::string name = PTree::reify(PTree::second(class_spec));
+    AST::Forward forward = my_ast_kit.create_forward(my_file, my_lineno, "C",
+						     type, AST::ScopedName(name));
+    add_comments(forward, class_spec->get_comments());
+    my_ast.declarations().append(forward);
+    return;
+  }  
+  if (size == 4) // struct definition
+  {
+    // [ class|struct <name> <inheritance> [{ body }] ]
+
+    std::string type = PTree::reify(PTree::first(class_spec));
+    std::string name = PTree::reify(PTree::second(class_spec));
+    PTree::nth(class_spec, 3)->accept(this);
+  }
+  else if (size == 3) // anonymous struct definition
+  {
+    // [ struct [nil nil] [{ ... }] ]
+
+  }
+}
+
+void ASTTranslator::visit(PTree::EnumSpec *enum_spec)
+{
+  Trace trace("ASTTranslator::visit(PTree::EnumSpec *)");
+  if (!PTree::second(enum_spec)) return; /* anonymous enum */
+  std::string name = PTree::reify(PTree::second(enum_spec));
+  update_position(enum_spec);
+
+  AST::Enumerators enumerators;
+  PTree::Node *enode = PTree::second(PTree::third(enum_spec));
+  AST::Enumerator enumerator;
+  while (enode)
+  {
+    update_position(enode);
+    PTree::Node *penumor = PTree::first(enode);
+    if (penumor->is_atom())
+    {
+      // Just a name
+      AST::ScopedName qname(PTree::reify(penumor));
+      enumerator = my_ast_kit.create_enumerator(my_file, my_lineno, "C",
+						qname, "");
+      add_comments(enumerator, static_cast<PTree::CommentedAtom *>(penumor)->get_comments());
+    }
+    else
+    {
+      // Name = Value
+      AST::ScopedName qname(PTree::reify(PTree::first(penumor)));
+      std::string value;
+      if (PTree::length(penumor) == 3)
+        value = PTree::reify(PTree::third(penumor));
+      enumerator = my_ast_kit.create_enumerator(my_file, my_lineno, "C",
+						qname, value);
+      add_comments(enumerator, static_cast<PTree::CommentedAtom *>(penumor)->get_comments());
+    }
+    enumerators.append(enumerator);
+    enode = PTree::rest(enode);
+    // Skip comma
+    if (enode && enode->car() && *enode->car() == ',') enode = PTree::rest(enode);
+  }
+  // Add a dummy enumerator at the end to absorb trailing comments.
+  PTree::Node *close = PTree::third(PTree::third(enum_spec));
+  enumerator = my_ast_kit.create_enumerator(my_file, my_lineno, "C",
+					    AST::ScopedName(std::string("dummy")), "");
+  add_comments(enumerator, static_cast<PTree::CommentedAtom *>(close));
+  enumerators.append(enumerator);
+  
+  // Create AST.Enum object
+  AST::Enum enum_ = my_ast_kit.create_enum(my_file, my_lineno, "C",
+					   name, enumerators);
+  add_comments(enum_, enum_spec);
+
+  my_ast.declarations().append(enum_);
 }
 
 void ASTTranslator::visit(PTree::Typedef *typed)
@@ -83,6 +193,90 @@ void ASTTranslator::visit(PTree::Typedef *typed)
     my_types.declare(qname, declaration);
     
     add_comments(declaration, declarator->get_comments());
+    my_ast.declarations().append(declaration);
+  }
+}
+
+void ASTTranslator::translate_parameters(PTree::Node *node,
+					 AST::TypeList types,
+					 AST::Function::Parameters &parameters)
+{
+  Trace trace("ASTTranslator::translate_parameters");
+
+  while (node)
+  {
+    // A parameter has a type, possibly a name and possibly a value.
+    // Treat the value as a string, i.e. don't analyse the expression further.
+    std::string name, value;
+    AST::Modifiers premods, postmods;
+    if (*node->car() == ',')
+      node = node->cdr();
+    PTree::Node *parameter = PTree::first(node);
+    AST::Type type = types.get(0);
+    types.del(0); // pop one value
+
+    // Discover contents. Ptree may look like:
+    //[register iostate [* a] = [0 + 2]]
+    //[register iostate [nil] = 0]
+    //[register iostate [nil]]
+    //[iostate [nil] = 0]
+    //[iostate [nil]]   etc
+    if (PTree::length(parameter) > 1)
+    {
+      // There is a parameter
+      int type_ix, value_ix = -1, len = PTree::length(parameter);
+      if (len >= 4 && *PTree::nth(parameter, len-2) == '=')
+      {
+        // There is an =, which must be followed by the value
+        value_ix = len-1;
+        type_ix = len-4;
+      }
+      else
+      {
+        // No =, so last is name and second last is type
+        type_ix = len-2;
+      }
+      // Skip keywords (eg: register) which are atoms
+      for (int ix = 0; ix < type_ix && PTree::nth(parameter, ix)->is_atom(); ++ix)
+      {
+        PTree::Node *atom = PTree::nth(parameter, ix);
+        premods.append(PTree::reify(atom));
+      }
+      // Find name
+      if (PTree::Node *n = PTree::nth(parameter, type_ix+1))
+      {
+        if (PTree::last(n) && !PTree::last(n)->is_atom() && 
+	    PTree::first(PTree::last(n)) &&
+            *PTree::first(PTree::last(n)) == ')' && PTree::length(n) >= 4)
+        {
+          // Probably a function pointer type
+          // pname is [* [( [* convert] )] ( [params] )]
+          // set to [( [* convert] )] from example
+          n = PTree::nth(n, PTree::length(n) - 4);
+          if (n && !n->is_atom() && PTree::length(n) == 3)
+          {
+            // set to [* convert] from example
+            n = PTree::second(n);
+            if (n && PTree::second(n) && PTree::second(n)->is_atom())
+              name = PTree::reify(PTree::second(n));
+          }
+        }
+        else if (!n->is_atom() && PTree::last(n) && PTree::last(n)->car())
+        {
+          // * and & modifiers are stored with the name so we must skip them
+          PTree::Node *last = PTree::last(n)->car();
+          if (*last != '*' && *last != '&')
+            // The last node is the name:
+            name = PTree::reify(last);
+        }
+      }
+      // Find value
+      if (value_ix >= 0) value = PTree::reify(PTree::nth(parameter, value_ix));
+    }
+    AST::Parameter p = my_ast_kit.create_parameter(premods, type, postmods,
+						   name, value);
+    parameters.append(p);
+    node = PTree::rest(node);
   }
 }
 
