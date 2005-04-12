ff54294219d046513b66b511567280e954a52ca1
Stefan Seefeld <stefan@seefeld.name>
2005-04-12 20:16:28 +0000

    reimplement compiler emulation code

--- Synopsis/Parsers/Cpp/Emulator.py
+++ Synopsis/Parsers/Cpp/Emulator.py
@@ -1,229 +1,30 @@
 #
-# Copyright (C) 2002 Stephen Davies
-# Copyright (C) 2003 Stefan Seefeld
+# Copyright (C) 2005 Stefan Seefeld
 # All rights reserved.
 # Licensed to the public under the terms of the GNU LGPL (>= 2),
 # see the file COPYING for details.
 #
 
 import sys, os, os.path, re, string, stat, tempfile
-from Synopsis import Util
+from Synopsis.config import version
 
-# The filename where infos are stored
-user_emulations_file = '~/.synopsis/cpp_emulations'
 
-# The list of default compilers.
-default_cc_compilers = ['cc', 'gcc']
-default_cxx_compilers = ['c++', 'g++', 'cl']
+class TempFile:
+    # Use tempfile.NamedTemporaryFile once we can rely on Python 2.4
+    def __init__(self, suffix):
 
-# A cache of the compiler info, loaded from the emulations file or calculated
-# by inspecting available compilers
-compiler_infos = {}
+        self.name = tempfile.mktemp(suffix)
+        self.file = open(self.name, 'w')
+        self.file.close()
 
-# A map of failed compilers. This map is kept for the lifetype of Synopsis, so
-# you may have to restart it if a compiler is 'fixed'
-failed = {}
 
-# The temporary filename
-temp_filename = None
+    def __del__(self):
 
-def get_temp_file(language):
-    """Returns the temporary filename. The temp file is created, but is left
-    empty. Choose an extension appropriate for the language, as some compilers
-    (notably gcc) will guess the language based on it."""
-    global temp_filename
-    if temp_filename: return temp_filename
-    if language == 'C++':
-        temp_filename = tempfile.mktemp('.cc')
-    else:
-        temp_filename = tempfile.mktemp('.c')
-    f = open(temp_filename, 'w')
-    f.close()
-    return temp_filename
+        os.unlink(self.name)
 
-def cleanup_temp_file():
-    """Removes the temporary file and resets the filename"""
-    global temp_filename
-    if temp_filename:
-        os.unlink(temp_filename)
-        temp_filename = None
 
-class CompilerInfo:
-    """Info about one compiler.
-
-    @attr compiler The name of the compiler, typically the executable name,
-    which must either be in the path or given as an absolute pathname
-    @attr language The language the compiler is used for.
-    @attr is_custom True if this is a custom compiler - in which case it will
-    never be updated automatically.
-    @attr timestamp The timestamp of the compiler binary
-    @attr include_paths A list of strings indicating the include paths
-    @attr macros A list of string 2-tuples indicating key=value pairs for
-    macros. A value of '' (empty string) indicates an empty definition. A
-    value of None (not a string) indicates that the macro should be undefined.
-    """
-    def __init__(self, compiler, language, is_custom, timestamp, include_paths, macros):
-        self.compiler = compiler
-        self.language = language
-        self.is_custom = is_custom
-        self.timestamp = timestamp
-        self.include_paths = include_paths
-        self.macros = macros
-
-def get_fallback(preferred, is_first_time):
-    """Tries to return info from a fallback compiler, and prints a warning
-    message to the user, unless their preferred compiler was 'none'"""
-    if is_first_time and preferred != 'none':
-        print "Warning: The specified compiler (%s) could not be found."%(preferred,)
-        print "You may want to retry with the full pathname of the compiler"
-        print "or with it in your path. If you don't have this compiler, you"
-        print "will need to modify the C++ Parser part of your config file."
-    for compiler in ('g++', 'gcc', 'c++', 'cc'):
-        if compiler_infos.has_key(compiler):
-            if is_first_time:
-              print "Warning: Falling back to compiler '%s'"%(compiler,)
-            return compiler_infos[compiler]
-    if preferred != 'none':
-        print "Warning: Unable to fallback to a default compiler emulation."
-        print "Unless you have set appropriate paths, expect errors."
-    return None
-
-def get_compiler_info(language, compiler):
-    """Returns the compiler info for the given compiler. If non is
-    specified (''), return the first available one for the given language.
-    The info is returned as a CompilerInfo object, or None if the compiler
-    isn't found. 
-    """
-    global failed, compiler_infos
-    # Check if this compiler has already been found to not exist
-    if failed.has_key(compiler):
-        return get_fallback(compiler, 0)
-    # See if already found it
-    if len(compiler_infos) == 0:
-        # Try to load the emulations file
-        compiler_infos = load_compiler_infos()
-    # if no compiler was specified take first available one
-    for c in compiler_infos:
-        if compiler_infos[c].language == language:
-            compiler = c
-            break
-    # See if wanted compiler was in file
-    if compiler_infos.has_key(compiler):
-        info = compiler_infos[compiler]
-        if info.is_custom: return info
-        file_stamp = get_compiler_timestamp(compiler)
-        # If compiler hasn't changed since then, return cached info
-        if file_stamp and info.timestamp == file_stamp:
-            return info
-    elif compiler:
-        # Add compiler to map, but with a dummy value to indicate nothing is
-        # known about it
-        compiler_infos[compiler] = None
-    
-    # Regenerate infos
-    refresh_compiler_infos(compiler_infos)
-
-    # Cache results to disk
-    write_compiler_infos(compiler_infos)
-
-    if not compiler and compiler_infos:
-        for c in compiler_infos:
-            if compiler_infos[c].language == language:
-                compiler = c
-                break
-
-    # Return discovered info, if compiler was found
-    if compiler_infos.has_key(compiler):
-        return compiler_infos[compiler]
-    else:
-        return get_fallback(compiler, 1)
-
-def load_compiler_infos():
-    """Loads the compiler infos from a file"""
-    glob = {'__builtins__':{}, '__name__':'__main__', 'setattr':setattr}
-    filename = os.path.expanduser(user_emulations_file)
-    try: execfile(filename, glob, glob)
-    except IOError: return {}
-    if glob.has_key('infos'):
-        return glob['infos']
-    return {} 
-
-def get_compiler_timestamp(compiler):
-    """Returns the timestamp for the given compiler, or 0 if not found"""
-    path = os.getenv('PATH', os.defpath)
-    path = string.split(path, os.pathsep)
-    for directory in path:
-        # Try to stat the compiler in this directory, if it exists
-        filename = os.path.join(directory, compiler)
-        if os.name == 'nt': filename += '.exe'
-        try: stats = os.stat(filename)
-        except OSError: continue
-        return stats[stat.ST_CTIME]
-    # Not found
-    return 0
-
-def refresh_compiler_infos(infos):
-    """Refreshes the list of infos, by rediscovering all non-custom compilers
-    in the map. The map is modified in-place."""
-    global failed
-    # Refresh each non-custom compiler in the map 
-    for compiler, info in infos.items():
-        if info and info.is_custom:
-            # Skip custom compilers
-            continue
-        if failed.has_key(compiler):
-            # Skip compilers that have already failed
-            del infos[compiler]
-            continue
-        info = find_compiler_info(compiler)
-        if info: infos[compiler] = info
-        else:
-            del infos[compiler]
-            failed[compiler] = None
-    # Now try to add defaults
-    for compiler in default_cc_compilers + default_cxx_compilers:
-        # Don't retry already-failed compilers
-        if failed.has_key(compiler):
-            continue
-        info = find_compiler_info(compiler)
-        if info: infos[compiler] = info
-
-    # Cleanup the temp file
-    cleanup_temp_file()
-        
-
-def write_compiler_infos(infos):
-    filename = os.path.expanduser(user_emulations_file)
-    dirname = os.path.dirname(filename)
-    if not os.path.exists(dirname):
-        os.mkdir(dirname)
-    file = open(filename, 'wt')
-    writer = Util.PyWriter(file)
-    writer.write_top('"""This file contains info about compilers for use by '+
-    'Synopsis.\nSee the Synopsis RefManual for info on customizing this list."""\n')
-    writer.ensure_struct()
-    writer.write('infos = {\n')
-    writer.indent()
-    ccomma = 0
-    for compiler, info in infos.items():
-        if ccomma: writer.write(",\n")
-        else: ccomma = 1
-        writer.write("'%s' : struct(\n"%compiler)
-        writer.write("language = '%s',\n" % info.language)
-        writer.write('is_custom = %d,\n' % info.is_custom)
-        writer.write('timestamp = %d,\n' % info.timestamp)
-        writer.write("include_paths = ")
-        writer.write_long_list(info.include_paths)
-        writer.write(",\nmacros = ")
-        writer.write_long_list(info.macros)
-        writer.write(")")
-    writer.outdent()
-    writer.write("\n}\n")
-    writer.flush()
-    file.close()
-    
-def find_ms_compiler_info(compiler):
-    """Try to find a compiler on the windows platform.
+def find_ms_compiler_info():
+    """Try to find a (C++) MSVC compiler.
     Return tuple of include path list and macro dictionary."""
 
     vc6 = ('SOFTWARE\\Microsoft\\DevStudio\\6.0\\Products\\Microsoft Visual C++', 'ProductDir')
@@ -295,19 +96,17 @@ def find_ms_compiler_info(compiler):
         except:
             continue
 
-    return 'C++', paths, macros
+    return paths, macros
 
-def find_gcc_compiler_info(compiler):
-    # Run the compiler with -v and get the displayed info
-
-    if compiler in ['cc', 'gcc']:
-        language = 'C'
-    else:
-        language = 'Cxx'
+def find_gcc_compiler_info(language, compiler, arguments):
+    """Try to find a GCC-based C or C++ compiler.
+    Return tuple of include path list and macro dictionary."""
 
     paths, macros = [], []
-
-    command = 'LANG=en_US %s -E -v -dD %s'%(compiler, get_temp_file(language))
+    arguments = ' '.join(arguments)
+    temp = TempFile(language == 'C++' and '.cc' or '.c')
+    # The output below the en_US locale, so make sure we use that.
+    command = 'LANG=en_US %s %s -E -v -dD %s'%(compiler, arguments, temp.name)
     cin, out,err = os.popen3(command)
     lines = err.readlines()
     cin.close()
@@ -319,17 +118,6 @@ def find_gcc_compiler_info(compiler):
         if state == 0:
             if line[:11] == 'gcc version': state = 1
         elif state == 1:
-            # cpp command line
-            args = string.split(line)
-            for arg in args:
-                if arg[0] != '-':
-                    continue
-                if arg[1] == 'D':
-                    if arg.find('=') != -1:
-                        macros.append(tuple(string.split(arg[2:], '=', 1)))
-                    else:
-                        macros.append((arg[2:], ''))
-                # TODO: do we need the asserts?
             state = 2
         elif state == 2:
             if line == '#include <...> search starts here:':
@@ -339,49 +127,238 @@ def find_gcc_compiler_info(compiler):
                 state = 4
             else:
                 paths.append(line.strip())
-
     # now read built-in macros
     state = 0
     for line in out.readlines():
         line = line.rstrip()
         if state == 0:
-            if line == '# 1 "<built-in>"':
+            if line == '# 1 "<built-in>"' or line == '# 1 "<command line>"':
                 state = 1
         elif state == 1:
             if line.startswith('#define '):
                 tokens = line[8:].split(' ', 1)
                 if len(tokens) == 1: tokens.append('')
                 macros.append(tuple(tokens))
-            elif line == '# 1 "<command line>"':
-                state = 2
+            elif line == '# 1 "%s"'%temp:
+                state = 0
 
     out.close()
-        
+    
     # Per-compiler adjustments
     for name, value in tuple(macros):
         if name == '__GNUC__' and value == '2':
             # gcc 2.x needs this or it uses nonstandard syntax in the headers
             macros.append(('__STRICT_ANSI__', ''))
 
-    return language, paths, macros
+    return paths, macros
 
-def find_compiler_info(compiler):
-    print "Finding info for '%s' ..."%compiler,
+
+def find_compiler_info(language, compiler, arguments):
 
     paths, macros = [], []
 
     if compiler == 'cl' and os.name == 'nt':
-        language, paths, macros = find_ms_compiler_info(compiler)
+        if arguments:
+            sys.stderr.write('Warning: ignoring unknown arguments for MSVC compiler\n')
+        paths, macros = find_ms_compiler_info()
 
     else:
-        language, paths, macros = find_gcc_compiler_info(compiler)
+        paths, macros = find_gcc_compiler_info(language, compiler, arguments)
         
-    if not paths or not macros:
-        print "Failed"
-        return None
-    print "Found"
+    return paths, macros
 
-    timestamp = get_compiler_timestamp(compiler)
-    return CompilerInfo(compiler, language, 0, timestamp, paths, macros)
 
-   
+def get_compiler_timestamp(compiler):
+    """Returns the timestamp for the given compiler, or 0 if not found"""
+
+    path = os.getenv('PATH', os.defpath)
+    path = string.split(path, os.pathsep)
+    for directory in path:
+        # Try to stat the compiler in this directory, if it exists
+        filename = os.path.join(directory, compiler)
+        if os.name == 'nt': filename += '.exe'
+        try: stats = os.stat(filename)
+        except OSError: continue
+        return stats[stat.ST_CTIME]
+    # Not found
+    return 0
+
+
+class CompilerInfo:
+    """Info about one compiler.
+
+    @attr compiler The name of the compiler, typically the executable name,
+    which must either be in the path or given as an absolute pathname.
+    @attr language The language the compiler is used for.
+    @attr kind A string indicating the type of this info: one of 'system', 'custom', ''.
+    'custom' compilers will never be automatically updated, and an empty string indicates
+    a failure to look up the given compiler.
+    @attr timestamp The timestamp of the compiler binary
+    @attr include_paths A list of strings indicating the include paths
+    @attr macros A list of string 2-tuples indicating key=value pairs for
+    macros. A value of '' (empty string) indicates an empty definition. A
+    value of None (not a string) indicates that the macro should be undefined.
+    """
+    compiler = ''
+    arguments = []
+    language = ''
+    kind = ''
+    timestamp = ''
+    include_paths = []
+    macros = []
+
+    def _write(self, os):
+
+        item = id(self) >= 0 and id(self) or -id(self)
+        os.write('class Item%u:\n'%item)
+        for name, value in CompilerInfo.__dict__.items():
+            if name[0] != '_':
+                os.write('    %s=%r\n'%(name, getattr(self, name)))
+        os.write('\n')
+
+
+class CompilerList(object):
+
+    user_emulations_file = '~/.synopsis/cpp_emulations'
+    default_cc_compilers = ['cc', 'gcc']
+    default_cxx_compilers = ['c++', 'g++', 'cl']
+
+    def __init__(self, filename = ''):
+
+        self.compilers = []
+        self.load(filename)
+
+
+    def list(self):
+
+        return [c.compiler for c in self.compilers]
+
+
+    def _query(self, language, compiler, arguments):
+        """Construct and return a CompilerInfo object for the given compiler."""
+
+        ci = CompilerInfo()
+        ci.compiler = compiler
+        ci.arguments = arguments
+        ci.language = language
+        try:
+            paths, macros = find_compiler_info(language, compiler, arguments)
+            ci.kind = 'system'
+            ci.timestamp = get_compiler_timestamp(compiler)
+            ci.include_paths = paths
+            ci.macros = macros
+        except:
+            ci.kind = '' # failure
+            ci.timestamp = 0
+            ci.include_paths = []
+            ci.macros = []
+            
+        return ci
+    
+
+    def load(self, filename = ''):
+        """Loads the compiler infos from a file"""
+
+        compilers = []
+
+        glob = {}
+        glob['version'] = version
+        class Type(type):
+            """Factory for CompilerInfo objects.
+            This is used to read in a emulation file."""
+
+            def __init__(cls, name, bases, dict):
+
+                if glob['version'] == version:
+                    compiler = CompilerInfo()
+                    for name, value in CompilerInfo.__dict__.items():
+                        if name[0] != '_':
+                            setattr(compiler, name, dict.get(name, value))
+                    compilers.append(compiler)
+
+        if not filename:
+            filename = CompilerList.user_emulations_file
+        filename = os.path.expanduser(filename)
+        glob['__builtins__'] = __builtins__
+        glob['__name__'] = '__main__'
+        glob['__metaclass__'] = Type
+        try:
+            execfile(filename, glob, glob)
+        except IOError:
+            # start with some default compilers
+            compilers.append(self._query('C++', 'c++', []))
+            compilers.append(self._query('C++', 'g++', []))
+            compilers.append(self._query('C', 'cc', []))
+            compilers.append(self._query('C', 'gcc', []))
+        self.compilers = compilers
+        
+
+    def save(self, filename = ''):
+        
+        if not filename:
+            filename = CompilerList.user_emulations_file
+        filename = os.path.expanduser(filename)
+        dirname = os.path.dirname(filename)
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+        emu = open(filename, 'wt')
+        emu.write("""# This file was generated by Synopsis.Parsers.Cpp.Emulator.
+# When making any manual modifications to any of the classes
+# be sure to set the 'kind' field to 'custom' so it doesn't get
+# accidentally overwritten !""")
+        emu.write('# documentation...\n')
+        emu.write('\n')
+        emu.write('version=%r\n'%version)
+        emu.write('\n')
+        for c in self.compilers:
+            c._write(emu)
+        
+
+    def refresh(self):
+        """Refreshes the compiler list.
+        Regenerate all non-custom compilers without destroying custom
+        compilers."""
+
+        compilers = []
+        for ci in self.compilers:
+            if ci.is_custom:
+                compilers.append(ci)
+            ci = _query(ci.language, ci.compiler, ci.arguments)
+            if ci:
+                compilers.append(ci)
+                
+        self.compilers = compilers
+
+
+    def find(self, language, compiler, arguments):
+
+        if not arguments:
+            arguments = []
+        for ci in self.compilers:
+            if (not compiler and language == ci.language
+                or (compiler == ci.compiler and arguments == ci.arguments)):
+                return ci
+        print 'could not find anything for %s %s %s'%(language, compiler, arguments)
+        ci = self._query(language, compiler, arguments)
+        self.compilers.append(ci)
+        return ci
+
+
+# Cache that makes multiple calls to 'get_compiler_info' more efficient.
+compiler_list = None
+
+
+def get_compiler_info(language, compiler = '', arguments = None):
+    """Returns the compiler info for the given compiler. If non is
+    specified (''), return the first available one for the given language.
+    The info is returned as a CompilerInfo object, or None if the compiler
+    isn't found. 
+    """
+    global compiler_list
+
+    if not compiler_list:
+        compiler_list = CompilerList()
+
+    ci = compiler_list.find(language, compiler, arguments)
+    compiler_list.save()
+    return ci
