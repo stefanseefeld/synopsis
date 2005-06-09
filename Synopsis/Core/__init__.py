c852b9593be0f0ce5d5e971e9f06a9061d3b9ae9
Stefan Seefeld <stefan@seefeld.name>
2005-06-09 20:25:00 +0000

    Remove dead code.

--- Synopsis/Core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Package for core data types and utilities.
-
-The Core package contains the definitions of the AST and Type hierarchies, and
-some utility methods such as imports and scoped name manipulations."""
-# this file is intentionally left empty
-# -stefan
