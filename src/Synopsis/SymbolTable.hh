48a3c0300f617e48d0e48c6c48a28e1b197a598d
Stefan Seefeld <stefan@seefeld.name>
2005-06-24 15:08:16 +0000

    Refactoring Part 3

---
+++ src/Synopsis/SymbolTable.hh
@@ -0,0 +1,15 @@
+//
+// Copyright (C) 2004 Stefan Seefeld
+// All rights reserved.
+// Licensed to the public under the terms of the GNU LGPL (>= 2),
+// see the file COPYING for details.
+//
+#ifndef Synopsis_SymbolTable_hh_
+#define Synopsis_SymbolTable_hh_
+
+#include <Synopsis/SymbolTable/Symbol.hh>
+#include <Synopsis/SymbolTable/Scope.hh>
+#include <Synopsis/SymbolTable/Scopes.hh>
+#include <Synopsis/SymbolTable/Walker.hh>
+
+#endif
