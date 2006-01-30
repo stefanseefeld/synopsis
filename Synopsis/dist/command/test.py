415aa25dc10b2ae0d9abb047e3028a8b4c588c21
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2006-01-30 18:36:48 +0000

    Ignore qmtest's error code.

--- Synopsis/dist/command/test.py
+++ Synopsis/dist/command/test.py
@@ -7,6 +7,7 @@
 
 import os, sys, string
 
+from distutils.errors import *
 from distutils.core import setup
 from distutils.command.build_ext import build_ext
 from distutils.util import get_platform
@@ -21,6 +22,7 @@ class test(build_ext):
 
     user_options = build_ext.user_options[:] + [
         ('suite=', 's', "the id of a test suite to run."),
+        ('expectation=', 'e', "an expectation file to compare the results against."),
         ('interactive', None, "run the web iterface to the testing engine.")]
 
     boolean_options = build_ext.boolean_options[:] + ['interactive']
@@ -28,6 +30,7 @@ class test(build_ext):
     def initialize_options (self):
         build_ext.initialize_options(self)
         self.suite = ''
+        self.expectation = ''
         self.interactive = False
 
     def finalize_options (self):
@@ -36,14 +39,22 @@ class test(build_ext):
     def run(self):
 
         cwd = os.getcwd()
+        if self.expectation:
+            self.expectation = os.path.join(cwd, self.expectation)
         os.environ['QMTEST_CLASS_PATH'] = os.path.join(cwd, 'tests', 'QMTest')
         test_dir = os.path.join(self.build_temp, 'tests')
         os.chdir(test_dir)
 
-        command = "qmtest "
-        command += self.interactive and "gui " or "run "
+        command = 'qmtest '
+        command += self.interactive and 'gui ' or 'run '
+        if self.expectation:
+            command += '-O %s '%self.expectation
         command += self.suite
                 
         self.announce(command)
-        spawn(['sh', '-c', command], self.verbose, self.dry_run)
-        os.chdir(cwd)
+        try:
+            spawn(['sh', '-c', command], self.verbose, self.dry_run)
+        # As qmtest returns a non-zero exit code if some tests fail, we
+        # simply ignore exec errors here.
+        except DistutilsExecError, error:
+            pass
