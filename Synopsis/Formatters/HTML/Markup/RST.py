59cfe38c74e1f3eff38f6f347e99a823932dc816
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2006-04-06 12:39:27 +0000

    Add preliminary ReStructuredText support.

--- Synopsis/Formatters/HTML/Markup/RST.py
+++ Synopsis/Formatters/HTML/Markup/RST.py
@@ -7,41 +7,21 @@
 
 from Synopsis import AST, Type
 from Synopsis.Formatters.HTML.Tags import *
-from Formatter import Formatter
+from Synopsis.Formatters.HTML.Markup import *
 from docutils import nodes
 from docutils.nodes import NodeVisitor, Text
 from docutils.frontend import OptionParser
 from docutils.utils import new_reporter
+from docutils.core import *
 import string, re
 
 class RST(Formatter):
 
-    def format_summary(self, view, decl):
-        summary = decl.comments()[-1].summary
-        print 'this is a summary'
-        return summary and self.translate(summary) or ''
+    def format(self, decl, view):
 
-    def format(self, view, decl):
-        details = decl.comments()[-1].details
-        print 'xxx', decl.comments()[-1].text, details
-        return details and self.translate(details) or ''
+        doc = decl.annotations.get('doc')
+        if doc:
+            parts = publish_parts(doc.text, writer_name='html')
+            print parts.keys()
+        return Struct('', parts['html_body'])
 
-    def translate(self, document, directory=None, docindex=None, context=None):
-
-        print 'translating', document
-        if not document.reporter:
-            # We unset this before pickling, so we have to restore it now.
-            fe = OptionParser()
-            settings = fe.get_default_values()
-            settings.update({
-                'warning_stream': '',
-                'halt_level': 10000,
-                'report_level': 10000,
-                }, fe)
-            document.reporter = new_reporter('', settings)
-
-        #linker = Linker()
-        #visitor = Translator(document, linker,
-        #                     directory, docindex, context)
-        #document.walkabout(visitor)
-        #return ''.join(visitor.body)
