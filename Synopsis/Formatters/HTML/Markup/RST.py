43a384893d3e1d1f4b1a6f6a898643f9509462bf
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2006-04-06 15:12:04 +0000

    Cleanup.

--- Synopsis/Formatters/HTML/Markup/RST.py
+++ Synopsis/Formatters/HTML/Markup/RST.py
@@ -8,14 +8,54 @@
 from Synopsis import AST, Type
 from Synopsis.Formatters.HTML.Tags import *
 from Synopsis.Formatters.HTML.Markup import *
-from docutils import nodes
 from docutils.nodes import NodeVisitor, Text
-from docutils.frontend import OptionParser
-from docutils.utils import new_reporter
 from docutils.core import *
 import string, re
 
+class SummaryExtractor(NodeVisitor):
+    """A docutils node visitor that extracts the first sentence from
+    the first paragraph in a document."""
+
+    def __init__(self, document):
+
+        NodeVisitor.__init__(self, document)
+        self.summary = None
+        
+
+    def visit_document(self, node):
+
+        self.summary = None
+
+        
+    def visit_paragraph(self, node):
+
+        if self.summary is not None:
+            return
+
+        summary_pieces = []
+
+        # Extract the first sentence.
+        for child in node:
+            if isinstance(child, Text):
+                m = re.match(r'(\s*[\w\W]*?\.)(\s|$)', child.data)
+                if m:
+                    summary_pieces.append(Text(m.group(1)))
+                    break
+            summary_pieces.append(child)
+            
+        summary_doc = self.document.copy()
+        summary_doc[:] = summary_pieces
+        self.summary = summary_doc
+
+
+    def unknown_visit(self, node):
+        'Ignore all unknown nodes'
+
+        pass
+
+
 class RST(Formatter):
+
 
     def format(self, decl, view):
 
