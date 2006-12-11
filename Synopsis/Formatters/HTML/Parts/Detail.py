08bbcf2ae460c47642f4e49647f578565ea30d19
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2006-12-11 03:26:43 +0000

    Clean up Scope View formatting.

--- Synopsis/Formatters/HTML/Parts/Detail.py
+++ Synopsis/Formatters/HTML/Parts/Detail.py
@@ -13,46 +13,49 @@ from Synopsis.Formatters.HTML.Tags import *
 
 class Detail(Part):
 
-   fragments = Parameter([DeclarationDetailFormatter(), DetailCommenter()],
+    fragments = Parameter([DeclarationDetailFormatter(), DetailCommenter()],
                          '')
 
-   def write_section_start(self, heading):
-      """Creates a table with one row. The row has a td of class 'heading'
-      containing the heading string"""
+    def write_section_start(self, heading):
+        """Start a 'detail' section and write an appropriate heading."""
 
-      self.write('<table width="100%%" summary="%s">\n'%heading)
-      self.write('<tr><td colspan="2" class="heading">' + heading + '</td></tr>\n')
-      self.write('</table>')
+        self.write('<div class="detail">\n')
+        self.write('<div class="heading">%s</div>\n'%heading)
 
-   def write_section_item(self, text):
-      """Writes text and follows with a horizontal rule"""
+    def write_section_end(self, heading):
+        """Close the section."""
+        
+        self.write('</div><!-- detail -->\n')
+        
+    def write_section_item(self, text):
+        """Add an item."""
 
-      self.write(text + '\n')
+        self.write('<div class="item">%s</div>\n'%text)
 
-   def process(self, decl):
-      "Print out the details for the children of the given decl"
+    def process(self, decl):
+        "Print out the details for the children of the given decl"
 
-      doc = self.processor.documentation
-      sorter = self.processor.sorter
-      sorter.set_scope(decl)
-      sorter.sort_section_names()
+        doc = self.processor.documentation
+        sorter = self.processor.sorter
+        sorter.set_scope(decl)
+        sorter.sort_section_names()
 
-      # Iterate through the sections with details
-      self.write_start()
-      for section in sorter.sections():
-         # Write a heading
-         heading = section+' Details:'
-         started = 0 # Lazy section start incase no details for this section
-         # Iterate through the children in this section
-         for child in sorter.children(section):
-            # Check if need to add to detail list
-            if not doc.details(child, self.view()):
-               continue
-            # Check section heading
-            if not started:
-               started = 1
-               self.write_section_start(heading)
-            child.accept(self)
-         # Finish the section
-         if started: self.write_section_end(heading)
-      self.write_end()
+        # Iterate through the sections with details
+        self.write_start()
+        for section in sorter.sections():
+            # Write a heading
+            heading = section+' Details:'
+            started = 0 # Lazy section start incase no details for this section
+            # Iterate through the children in this section
+            for child in sorter.children(section):
+                # Check if need to add to detail list
+                if not doc.details(child, self.view()):
+                    continue
+                # Check section heading
+                if not started:
+                    started = 1
+                    self.write_section_start(heading)
+                child.accept(self)
+            # Finish the section
+            if started: self.write_section_end(heading)
+        self.write_end()
--- Synopsis/Formatters/HTML/Parts/Summary.py
+++ Synopsis/Formatters/HTML/Parts/Summary.py
@@ -13,86 +13,81 @@ from Synopsis.Formatters.HTML.Fragments import *
 from Synopsis.Formatters.HTML.Tags import *
 
 class Summary(Part):
-   """Formatting summary visitor. This formatter displays a summary for each
-   declaration, with links to the details if there is one. All of this is
-   controlled by the ASTFormatters."""
+    """Formatting summary visitor. This formatter displays a summary for each
+    declaration, with links to the details if there is one. All of this is
+    controlled by the ASTFormatters."""
 
-   fragments = Parameter([DeclarationSummaryFormatter(), SummaryCommenter()],
+    fragments = Parameter([DeclarationSummaryFormatter(), SummaryCommenter()],
                          '')
 
-   def register(self, view):
+    def register(self, view):
 
-      if view.processor.has_view('XRef'):
-         self.fragments.append(XRefLinker())
-      if view.processor.has_view('Source'):
-         self.fragments.append(SourceLinker())
+        if view.processor.has_view('XRef'):
+            self.fragments.append(XRefLinker())
+        if view.processor.has_view('Source'):
+            self.fragments.append(SourceLinker())
+            
+        Part.register(self, view)
+        self.__link_detail = 0
 
-      Part.register(self, view)
-      self.__link_detail = 0
+    def set_link_detail(self, boolean):
+        """Sets link_detail flag to given value.
+        @see label()"""
 
-   def set_link_detail(self, boolean):
-      """Sets link_detail flag to given value.
-      @see label()"""
+        self.__link_detail = boolean
 
-      self.__link_detail = boolean
+    def label(self, ref, label=None):
+        """Override to check link_detail flag. If it's set, returns a reference
+        instead - which will be to the detailed info"""
 
-   def label(self, ref, label=None):
-      """Override to check link_detail flag. If it's set, returns a reference
-      instead - which will be to the detailed info"""
-
-      if label is None: label = ref
-      if self.__link_detail:
-         # Insert a reference instead
-         return span('name',self.reference(ref, Util.ccolonName(label, self.scope())))
-      return Part.label(self, ref, label)
+        if label is None: label = ref
+        if self.__link_detail:
+            # Insert a reference instead
+            return span('name',self.reference(ref, Util.ccolonName(label, self.scope())))
+        return Part.label(self, ref, label)
 	
-   def write_section_start(self, heading):
-      """Starts a table entity. The heading is placed in a row in a td with
-      the class 'heading'."""
+    def write_section_start(self, heading):
+        """Start a 'summary' section and write an appropriate heading."""
 
-      self.write('<table width="100%%" summary="%s">\n'%heading)
-      self.write('<col/><col width="100%" />')
-      self.write('<tr><td class="heading" colspan="2">' + heading + '</td></tr>\n')
+        self.write('<div class="summary">\n')
+        self.write('<div class="heading">%s</div>\n'%heading)
 
-   def write_section_end(self, heading):
-      """Closes the table entity and adds a break."""
+    def write_section_end(self, heading):
+        """Close the section."""
+        
+        self.write('</div><!-- summary -->\n')
 
-      self.write('</table>\n<br/>\n')
+    def write_section_item(self, text):
+        """Add an item."""
 
-   def write_section_item(self, text):
-      """Adds a table row"""
-
-      # fragment formatters place a <tab> as separator, so we can replace
-      # it with proper html markup here
-      #text = re.sub('\t', '</td><td class="summ-info">', text)
-      self.write('<tr><td/><td class="summ-info">' + text + '</td></tr>\n')
+        self.write('<div class="item">%s</div>\n'%text)
       
-   def process(self, scope):
-      "Print out the summaries from the given scope"
+    def process(self, scope):
+        "Print out the summaries from the given scope"
 
-      doc = self.processor.documentation
-      sorter = self.processor.sorter
-      sorter.set_scope(scope)
-      sorter.sort_section_names()
+        doc = self.processor.documentation
+        sorter = self.processor.sorter
+        sorter.set_scope(scope)
+        sorter.sort_section_names()
 
-      self.write_start()
-      for section in sorter.sections():
-         # Write a header for this section
-         if section[-1] == 's': heading = section+'es Summary:'
-         else: heading = section+'s Summary:'
-         self.write_section_start(heading)
-         # Iterate through the children in this section
-         for child in sorter.children(section):
-            # Check if need to add to detail list
-            if doc.details(child, self.view()):
-               # Setup the linking stuff
-               self.set_link_detail(1)
-               child.accept(self)
-               self.set_link_detail(0)
-            else:
-               # Just do it
-               child.accept(self)
-         # Finish off this section
-         self.write_section_end(heading)
-      self.write_end()
+        self.write_start()
+        for section in sorter.sections():
+            # Write a header for this section
+            if section[-1] == 's': heading = section+'es Summary:'
+            else: heading = section+'s Summary:'
+            self.write_section_start(heading)
+            # Iterate through the children in this section
+            for child in sorter.children(section):
+                # Check if need to add to detail list
+                if doc.details(child, self.view()):
+                    # Setup the linking stuff
+                    self.set_link_detail(1)
+                    child.accept(self)
+                    self.set_link_detail(0)
+                else:
+                    # Just do it
+                    child.accept(self)
+            # Finish off this section
+            self.write_section_end(heading)
+        self.write_end()
 
