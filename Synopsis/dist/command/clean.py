c3dd47c23c30bf33baaabc95a46dff712ed07dd4
Stefan Seefeld <stefan@seefeld.name>
2007-03-04 20:15:21 +0000

    Fix installation / packaging inconsistencies.

--- Synopsis/dist/command/clean.py
+++ Synopsis/dist/command/clean.py
@@ -42,22 +42,22 @@ class clean(base):
             for d in [libdir, incdir, bindir]:
                 if os.path.exists(d):
                     remove_tree(d, dry_run=self.dry_run)
-            pc = os.path.join(self.build_clib, 'Synopsis.pc')
+            pc = os.path.join(self.build_clib, 'synopsis.pc')
             if os.path.exists(pc) and not self.dry_run:
                 os.remove(pc)
 
             # Remove the generated documentation.
-            prefix = 'share/doc/Synopsis/html/Manual'
+            prefix = 'share/doc/synopsis/html/Manual'
             for d in ['cxx', 'python', 'sxr']:
                 if os.path.exists(os.path.join(prefix, d)):
                     remove_tree(os.path.join(prefix, d), dry_run=self.dry_run)
 
-            prefix = 'share/doc/Synopsis/html'
+            prefix = 'share/doc/synopsis/html'
             for d in ['Tutorial', 'DevGuide']:
                 if os.path.exists(os.path.join(prefix, d)):
                     remove_tree(os.path.join(prefix, d), dry_run=self.dry_run)
 
-            prefix = 'share/doc/Synopsis'
+            prefix = 'share/doc/synopsis'
             if os.path.exists(os.path.join(prefix, 'examples')):
                 remove_tree(os.path.join(prefix, 'examples'), dry_run=self.dry_run)
 
