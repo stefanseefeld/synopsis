96f49e6d6cb3cace5e717544796503cc2b29f929
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2008-03-17 05:40:46 +0000

    Various enhancements on documentation.

--- doc/Tutorial/examples/Paths/src/Bezier.h
+++ doc/Tutorial/examples/Paths/src/Bezier.h
@@ -8,8 +8,10 @@ namespace Paths
 {
 
 /**
- * The Bezier class. It implements a bezier curve
+ * The Bezier class. It implements a Bezier curve
  * for the given order.
+ * (@see http://en.wikipedia.org/wiki/B%C3%A9zier_curve for more
+ *  on Bezier curves.)
  */
 template <size_t Order>
 class Bezier : public Path
