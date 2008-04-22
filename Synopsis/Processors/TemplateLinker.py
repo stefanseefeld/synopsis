367c30881565c65c212b79a3cf0e9712ba4f5f56
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2008-04-22 12:49:14 +0000

    Make TemplateLinker idempotent.

--- Synopsis/Processors/TemplateLinker.py
+++ Synopsis/Processors/TemplateLinker.py
@@ -9,7 +9,7 @@ from Synopsis.Processor import *
 from Synopsis import ASG
 
 class TemplateLinker(Processor, ASG.Visitor):
-    """Link template specializations to their primary templates (and vice versa."""
+    """Link template specializations to their primary templates, and vice versa."""
 
     def process(self, ir, **kwds):
 
@@ -26,7 +26,8 @@ class TemplateLinker(Processor, ASG.Visitor):
             primary_name = d.name[:-1] + (d.name[-1].split('<')[0],)
             primary = self.ir.asg.types[primary_name]
             d.primary_template = primary_name
-            if type(primary) is ASG.DeclaredTypeId:
+            if (type(primary) is ASG.DeclaredTypeId and
+                d.name not in primary.declaration.specializations):
                 primary.declaration.specializations.append(d.name)
 
     visit_forward = link
