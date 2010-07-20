4992bf8745fdb477331f0ffe2eacee4b6dabb715
stefan <stefan@91adf076-48d4-0310-a9e2-da78070693fa>
2010-07-20 17:18:21 +0000

    Mask more useless declarations.

--- Synopsis/Formatters/HTML/Views/NameIndex.py
+++ Synopsis/Formatters/HTML/Views/NameIndex.py
@@ -113,7 +113,7 @@ class NameIndex(View):
         # or local variables. There are just too many of them...
         # typenames are not referring to actual (declared) types,
         # so aren't meaningful here either.
-        if type not in ['parameter', 'local variable', 'typename']:
+        if type not in ['parameter', 'local variable', 'typename', 'this']:
             name = self.reference(name, (), realname, title=title)+' '+type
             self.write(div(name, class_='nameindex-item'))
 
